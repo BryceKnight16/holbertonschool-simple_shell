180ba23d4834deb3be73765633c14728201558b0
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-26 16:01:13 +1000

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -6,36 +6,7 @@
  * @num_tokens: Pointer to store the number of tokens
  * Return: A NULL-terminated array of tokens
  */
-// char **tokenize_input(char *input, int *num_tokens)
-// {
-//     const char *delim = " \n";
-//     char *token;
-//     int i;
-//     char **argv = NULL;
-    
-//     *num_tokens = 0;
 
-//     token = strtok(input, delim);
-//     while (token != NULL)
-//     {
-//         (*num_tokens) ++;
-//         token = strtok(NULL, delim);
-//     }
-//      (*num_tokens) ++;
-
-//     argv = malloc(sizeof(char *) * (*num_tokens));
-//     token = strtok(input, delim);
-//     for (i = 0; token != NULL; i++)
-//     {
-//         argv[i] = strdup(token);
-//         token = strtok(NULL, delim);
-//     }
-//     printf("%s", argv[0]);
-//     printf("%s", argv[1]);
-//     argv[i] = NULL;
-
-//     return argv;
-// }
 char **tokenize_input(char *input, int *num_tokens)
 {
     const char *delim = " ";
