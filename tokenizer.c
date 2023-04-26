76df8375440a201b02ef7326637253970c407985
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-26 17:22:05 +1000

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -18,18 +18,20 @@ char **tokenize_input(char *input, int *num_tokens)
     token = strtok(input, delim);
     while (token != NULL)
     {
-        (*num_tokens) ++;
+        *num_tokens = num_tokens + 1;
         token = strtok(NULL, delim);
     }
-     (*num_tokens) ++;
+     *num_tokens = num_tokens + 1;
 
     argv = malloc(sizeof(char *) * (*num_tokens));
     token = strtok(input, delim);
     for (i = 0; token != NULL; i++)
     {
         argv[i] = strdup(token);
+        printf(" argv[%d] %s\n", i, argv[i]);
         token = strtok(NULL, delim);
     }
+    
     argv[i] = NULL;
 
     return argv;
