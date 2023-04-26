84b87050341bf016d8e9055326321224f39fbfbe
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-26 20:07:07 +1000

    Update free_list.c

--- free_list.c
+++ free_list.c
@@ -7,11 +7,15 @@
  */
 void free_list(list_t *head)
 {
-	if (head)
-	{
-		free_list(head->next);
-		if (head->str)
-			free(head->str);
-		free(head);
-	}
+    list_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp->str);
+        temp->next = NULL;
+        free(temp);
+    }
 }
+
