638c87b280527a50fc48d73cc87bc09bd7f4985f
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-26 16:05:35 +1000

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -9,14 +9,13 @@
 
 char **tokenize_input(char *input, int *num_tokens)
 {
-    const char *delim = " ";
+    const char *delim = " \n";
     char *token;
     int i;
-    char **argv;
-
-    input[strlen(input)-1] = '\0'; 
+    char **argv = NULL;
 
     *num_tokens = 0;
+
     token = strtok(input, delim);
     while (token != NULL)
     {
@@ -26,25 +25,13 @@ char **tokenize_input(char *input, int *num_tokens)
     (*num_tokens)++;
 
     argv = malloc(sizeof(char *) * (*num_tokens));
-    if (argv == NULL) {
-        fprintf(stderr, "Error: malloc failed\n");
-        return NULL;
-    }
-
     token = strtok(input, delim);
     for (i = 0; token != NULL; i++)
     {
         argv[i] = strdup(token);
-        if (argv[i] == NULL) {
-            fprintf(stderr, "Error: malloc failed\n");
-            return NULL;
-        }
         token = strtok(NULL, delim);
     }
-    printf("%s", argv[0]);
-    printf("%s", argv[1]);
     argv[i] = NULL;
 
     return argv;
 }
-
