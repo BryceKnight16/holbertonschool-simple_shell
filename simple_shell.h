2d2ac2975ae39a518d8b931717edb2b3ac11201d
BryceKnight16 <6373@holbertonstudents.com>
2023-04-26 03:16:09 -0700

    updating all files

--- simple_shell.h
+++ simple_shell.h
@@ -14,13 +14,14 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t list_len(const list_t *h);
 size_t print_list(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
 char *read_input(void);
 list_t *tokenize_input(char *input);
 void execute_command(char **argv);
-void free_list(list_t *head)
+
 
 
 #endif /* SIMPLE_SHELL_H */
