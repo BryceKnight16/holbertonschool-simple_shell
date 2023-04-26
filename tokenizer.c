d58495d049d705b6425661fbd588cb7cd86cee06
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-26 16:09:53 +1000

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,30 +1,42 @@
 #include "simple_shell.h"
 
 /**
+ * count_tokens - Counts the number of tokens in a string
+ * @str: The input string
+ * @delim: The delimiter string
+ * Return: The number of tokens
+ */
+int count_tokens(char *str, const char *delim)
+{
+    int num_tokens = 0;
+    char *token;
+
+    token = strtok(str, delim);
+    while (token != NULL)
+    {
+        num_tokens++;
+        token = strtok(NULL, delim);
+    }
+
+    return num_tokens;
+}
+
+/**
  * tokenize_input - Tokenizes the input string
  * @input: The input string
  * @num_tokens: Pointer to store the number of tokens
  * Return: A NULL-terminated array of tokens
  */
-
 char **tokenize_input(char *input, int *num_tokens)
 {
     const char *delim = " \n";
     char *token;
     int i;
-    char **argv = NULL;
+    char **argv;
 
-    *num_tokens = 0;
+    *num_tokens = count_tokens(input, delim);
+    argv = malloc(sizeof(char *) * (*num_tokens + 1));
 
-    token = strtok(input, delim);
-    while (token != NULL)
-    {
-        (*num_tokens)++;
-        token = strtok(NULL, delim);
-    }
-    (*num_tokens)++;
-
-    argv = malloc(sizeof(char *) * (*num_tokens));
     token = strtok(input, delim);
     for (i = 0; token != NULL; i++)
     {
@@ -35,3 +47,4 @@ char **tokenize_input(char *input, int *num_tokens)
 
     return argv;
 }
+
