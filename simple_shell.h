1517d30d6268cf38f3b8fadc7ec6b0ad3d7cc366
BryceKnight16 <124347057+BryceKnight16@users.noreply.github.com>
2023-04-26 20:02:11 +1000

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -20,6 +20,7 @@ list_t *add_node_end(list_t **head, const char *str);
 char *read_input(void);
 list_t *tokenize_input(char *input);
 void execute_command(char **argv);
-void free_memory(char **argv, int num_tokens, char *str_parse);
+void free_list(list_t *head)
+
 
 #endif /* SIMPLE_SHELL_H */
