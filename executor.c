e7c6b1c1e8924bd714539f4a319fee2f1dc43655
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-28 10:25:33 -0700

    find ./hsh

--- executor.c
+++ executor.c
@@ -125,11 +125,12 @@ void execute_command(char **argv)
 {
 	pid_t pid;
 	char *full_path = NULL;
+	char *program_name = "./hsh";
 
 	full_path = search_path(argv[0]);
 	if (full_path == NULL)
 	{
-		fprintf(stderr, "%s: 1: %s: not found\n", argv[2], argv[0]);
+		fprintf(stderr, "%s: 1: %s: not found\n", program_name, argv[0]);
 		exit(127);
 	}
 
