2ebe28e64d6b15be1df6c322f8ecbfb2c59dbde2
Sourabh Beniwal <sourabhbeniwal@outlook.com>
2023-04-29 15:54:18 +1000

    Create printing.c
    added print env

---
+++ printing.c
@@ -0,0 +1,16 @@
+#include "simple_shell.h"
+
+/**
+ * print_env - Prints the current environment variables
+ */
+void print_env(void)
+{
+    int i;
+
+    i = 0;
+    while (environ[i] != NULL)
+    {
+        printf("%s\n", environ[i]);
+        i++;
+    }
+}
